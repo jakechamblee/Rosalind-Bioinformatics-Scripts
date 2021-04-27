017cbb321ccf0333d23e8dd064ca3c1e0792f8d2
jcham <jchamblee1995@gmail.com>
2021-04-27 17:39:17 -0500

    Add profile_most_probable_kmer

--- chapter1_and_chapter2/profile_most_probable_kmer.py
+++ chapter1_and_chapter2/profile_most_probable_kmer.py
@@ -6,37 +6,44 @@ def profile_most_probable_kmer(text: str, k: int, profile: list):
 
     i.e. the kmer that is most likely to be generated in text, given the profile
     """
+    from math import prod
+
     profile: Dict[str, int] = parse_profile_input_to_list(profile)
     # find all possible kmers in text of size k
     kmers: set = find_all_possible_kmers(text, k)
     # find the probability of each kmer given profile
+    lowest_kmer_probability = 0
+    most_likely_kmer = None
+
     for kmer in kmers:
+        kmer_probabilities = []
+
         for i in range(k):
             base_at_i: str = kmer[i]
-            probability_of_base_given_profile = profile[base_at_i][i]
+            probability_of_base_at_position_i_given_profile = profile[base_at_i][i]
+            kmer_probabilities.append(probability_of_base_at_position_i_given_profile)
 
-    # return the most probable kmer
+        kmer_probability: float = prod(kmer_probabilities)
 
-    return None
+        if kmer_probability > lowest_kmer_probability:
+            lowest_kmer_probability = kmer_probability
+            most_likely_kmer = kmer
+
+    return most_likely_kmer
 
 
 def find_all_possible_kmers(text: str, k: int) -> set:
-    from chapter1_and_chapter2.findclumps import frequencytable
-
-    kmers_in_text = set()
-    for line in text:
-        kmers_in_line: List[str] = [key for key in frequencytable(line, k).keys()]
-        for kmer in kmers_in_line:
-            kmers_in_text.add(kmer)
+    from findclumps import frequencytable
+    kmers_in_text: dict = frequencytable(text, k)
 
     return kmers_in_text
 
 
-def parse_profile_input_to_list(prof_split: str) -> dict[str, list[float]]:
+def parse_profile_input_to_list(prof_split: str) -> dict:
     prof_split = prof_split.splitlines()
     prof_split = [i.split(' ') for i in prof_split]
-    alanine, guanine = {'A': [float(i) for i in prof_split[0]]}, {'G': [float(i) for i in prof_split[1]]}
-    cytosine, thymine = {'C': [float(i) for i in prof_split[2]]}, {'T': [float(i) for i in prof_split[3]]}
+    alanine, cytosine = {'A': [float(i) for i in prof_split[0]]}, {'C': [float(i) for i in prof_split[1]]}
+    guanine, thymine = {'G': [float(i) for i in prof_split[2]]}, {'T': [float(i) for i in prof_split[3]]}
 
     # merge the dictionaries
     new_profile = {**alanine, **guanine, **cytosine, **thymine}
@@ -51,4 +58,5 @@ if __name__ == '__main__':
 0.3 0.3 0.5 0.2 0.4
 0.1 0.2 0.1 0.1 0.2'''
     print(profile_most_probable_kmer(txt, 5, prof))
-    print(parse_profile_input_to_list(prof))
+    #print(parse_profile_input_to_list(prof))
+    #print(find_all_possible_kmers(txt, 5))
